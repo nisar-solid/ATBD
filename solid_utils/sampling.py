fe66f296c04c918a379fc99fd103f0b16da1b05b
rzinke <rzinke@jpl.caltech.edu>
2022-08-09 18:39:25 -0700

    Updated Secular Method to requirement of 2mm/yr and added profile function to sampling.py

--- solid_utils/sampling.py
+++ solid_utils/sampling.py
@@ -1,7 +1,13 @@
 import numpy as np
 import math
 import warnings
+
 from solid_utils.variogram import remove_trend
+from mintpy.utils import utils as ut
+
+
+# Seed random number generator for consistency
+np.random.seed(1)
 
 
 def load_geo(attr_geo):
@@ -80,6 +86,45 @@ def rand_samp(data:np.ndarray,X:np.ndarray,Y:np.ndarray,num_samples:int=10000):
     return sampled_data, sampled_X, sampled_Y
 
 
+def samps_to_pairs(x,y,data):
+    """Create pairs of data samples. If the lengths of x, y, and data
+    is N, then each array will be formatted into an (N/2, 2) array.
+
+    Parameters:
+    x: np.ndarray
+        input X location of the data points (1d)
+    y: np.ndarray
+        input Y location of the data points (1d)
+    data: np.ndarray
+        input data array (1d)
+
+    Returns:
+    x_pairs: np.ndarray
+        pairs of x points (2d)
+    y_pairs: np.ndarray
+        pairs of y points (2d)
+    data_pairs: np.ndarray
+        pairs of data points (2d)
+    """
+    x_odd = x[1::2]
+    y_odd = y[1::2]
+    data_odd = data[1::2]
+    if (x.shape[0] % 2) == 0:
+        x_even = x[0::2]
+        y_even = y[0::2]
+        data_even = data[0::2]
+    else:
+        x_even = x[0:-1:2]
+        y_even = y[0:-1:2]
+        data_even = data[0:-1:2]
+
+    x_pairs = np.column_stack([x_odd, x_even])
+    y_pairs = np.column_stack([y_odd, y_even])
+    data_pairs = np.column_stack([data_odd, data_even])
+
+    return x_pairs, y_pairs, data_pairs
+
+
 def pair_up(x,y,data):
     """Pair up data samples.
 
@@ -97,19 +142,25 @@ def pair_up(x,y,data):
     rel_measure: np.ndarray
         absolute value of data difference for every data pairs (1d)
     """
-    x_odd = x[1::2]
-    y_odd = y[1::2]
-    data_odd = data[1::2]
-    if (x.shape[0] % 2) == 0:
-        x_even = x[0::2]
-        y_even = y[0::2]
-        data_even = data[0::2]
-    else:
-        x_even = x[0:-1:2]
-        y_even = y[0:-1:2]
-        data_even = data[0:-1:2]
+    # Pair up samples
+    x_pairs, y_pairs, data_pairs = samps_to_pairs(x,y,data)
+
+    # Parse paired sample coordinates
+    x_odd = x_pairs[:,0]
+    x_even = x_pairs[:,1]
+
+    y_odd = y_pairs[:,0]
+    y_even = y_pairs[:,1]
+
+    data_odd = data_pairs[:,0]
+    data_even = data_pairs[:,1]
+
+    # Calculate distance between points
     distance = np.sqrt((x_odd-x_even)**2+(y_odd-y_even)**2)
+
+    # Calculate difference between measurements
     rel_measure = abs(data_odd-data_even)
+
     return distance,rel_measure
 
 
@@ -157,3 +208,116 @@ def samp_pair(x,y,data,num_samples=1000000,deramp=False):
     # pair up
     distance, rel_measure = pair_up(x1d,y1d,data1d)
     return distance, rel_measure
+
+
+def profile_samples(x, y, data, metadata, len_rqmt, num_samples=10000):
+    """Similar to samp_pair, randomly select data points and
+    retrieve a profile (transection) between those points.
+    Fit the data profile with a 1st order polynomial, and solve
+    that for the predicted end points.
+
+    Parameters:
+    X: np.ndarray
+        input longitudes of the data points (1d)
+    Y: np.ndarray
+        input latitudes of the data points(1d)
+    data: np.ndarray
+        input data array (1d)
+    metadata: dict
+        MintPy metadata describing spatial parameters of data set
+    len_rqmt: list
+        list of [<min>, <max>]-allowable profile distances, per ATBD
+        requirement
+    num_samples: int
+        number of points to be sampled, defalt value is 10,000
+
+    Returns:
+    distance: np.ndarray
+        distances for every data pair (1d)
+    rel_measure: np.ndarray
+        absolute value of data difference for every data pair (1d)
+    """
+    # mask nan, result in 1d
+    data1d = data.reshape(-1,1)
+    mask = np.isnan(data1d)
+    data1d = data1d[~mask]
+    x1d = x[~mask]
+    y1d = y[~mask]
+
+    # Draw random samples based on geographic coordinates
+    data1d, x1d, y1d = rand_samp(data1d, x1d, y1d, 2*num_samples)
+
+    # Reshape sample points into pairs (x1, x2), (y1, y2)
+    x_pairs, y_pairs, data_pairs = samps_to_pairs(x1d, y1d, data1d)
+
+    # Determine distances between pairs to check against length requirements
+    dists = haversine_distance(\
+                                x_pairs[:,0], y_pairs[:,0],
+                                x_pairs[:,1], y_pairs[:,1]\
+                                )
+    valid_indices = (dists > len_rqmt[0]) & (dists < len_rqmt[1])
+
+    # Filter for data within length requirements
+    x_pairs = x_pairs[valid_indices]
+    y_pairs = y_pairs[valid_indices]
+    data_pairs = data_pairs[valid_indices]
+
+    # Number of valid sample points
+    num_valid_samples = np.sum(valid_indices)
+
+    print('{:d} / {:d} profiles have valid lengths (0.1 - 50 km)'.\
+        format(num_valid_samples, num_samples))
+
+    # Loop through sample pairs
+    dist = np.empty(num_valid_samples)
+    rel_measure = np.empty(num_valid_samples)
+    for i in range(num_valid_samples):
+        # Retreive transect
+        start_lalo = [y_pairs[i,0], x_pairs[i,0]]
+        end_lalo = [y_pairs[i,1], x_pairs[i,1]]
+        transect = ut.transect_lalo(data, metadata, start_lalo, end_lalo,
+                interpolation='nearest')
+
+        # Convert distance from m to km
+        transect['distance'] /= 1000
+
+        # Solve for fit
+        fit = np.polyfit(transect['distance'], transect['value'], 1)
+
+        # Solve for values at profile start and end points
+        endpoint_data = np.poly1d(fit)([0, transect['distance'].max()])
+
+        # Compute abosolute difference between end points
+        abs_measurement_diff = np.abs(np.diff(endpoint_data)[0])
+
+        # Store distance and difference values
+        dist[i] = transect['distance'].max()
+        rel_measure[i] = abs_measurement_diff
+
+    return dist, rel_measure
+
+
+def haversine_distance(start_lon, start_lat, end_lon, end_lat):
+    """Compute the distance between two points geographic points using
+    the haversine formula.
+    """
+    # Convert coordinate degrees to radians
+    start_lat = np.deg2rad(start_lat)
+    start_lon = np.deg2rad(start_lon)
+
+    end_lat = np.deg2rad(end_lat)
+    end_lon = np.deg2rad(end_lon)
+
+    # Apply haversine formula
+    delta_lat = end_lat - start_lat
+    delta_lon = end_lon - start_lon
+
+    a = np.sin(delta_lat/2)**2 \
+        + np.cos(start_lat) \
+        * np.cos(end_lat) \
+        * np.sin(delta_lon/2)**2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    r = 6378  # Earth's radius
+
+    return c * r
